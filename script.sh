e7aa76b9dcd87ddd1f943c156482af390cc14b6c
miradox <pignier.baptiste@gmail.com>
2020-08-19 03:42:44 +0200

    change

--- script.sh
+++ script.sh
@@ -4,13 +4,17 @@
 export PATH=$PATH:$ANDROID_HOME/build-tools/29.0.3
 export PATH=$PATH:/opt/kotlinc/bin/
 
-SeeTemporaryFolder=true
+SeeTemporaryFolder=false
+
+#Change if you whant but it's pointless
 Storepass=Passwrd
 
+echo "CLEANING"
 rm -fr obj
 rm -fr bin
 rm -fr gen
 
+#Create keystore if you dont have one
 if [ ! -f MainKeystore.keystore ]; then
     echo "Lets generate a keystore to sign your apk : "
     keytool -genkeypair -v -storetype pkcs12 -dname "cn=A, ou=B, o=C, c=D" -keystore MainKeystore.keystore -storepass $Storepass -alias MainKey -keyalg RSA -keysize 2048 -validity 10000
@@ -27,20 +31,20 @@ if ! $SeeTemporaryFolder ; then
 	rm -f libs/constraint-layout-1.1.3-res.zip
 fi
 
-
+#Find all usefull files
 for i in libs/*.jar; do
   libs="$libs:$i"
   dxlibs="$dxlibs $i"
   d8libs="$d8libs --classpath $i"
 done
-
 libs="${libs#:}"
 all_R_java=$(find gen -type f -name '*.java') #Find all R.java (from lib and src) and .java files
 all_kotlin=$(find src -type f -name '*.kt')
 all_code_java=$(find src -type f -name '*.java')
 
-mkdir -p obj
+
 echo "JAVAC"
+mkdir -p obj
 javac -bootclasspath android.jar -d obj/ -classpath $libs -sourcepath gen:src $all_R_java $all_code_java
 
 
@@ -55,26 +59,24 @@ fi
 
 echo "D8"
 jar cf obj/all.jar obj/
-
 d8 --release $d8libs --lib android.jar --output bin/ obj/all.jar $dxlibs
 
 if ! $SeeTemporaryFolder ; then
 	rm -fr obj/
 fi
+
 echo "ZIP"
 zip -uj bin/AndroidTest.unsigned.unalign.apk bin/classes.dex 
 
 echo "ZIPALIGN"
-
 zipalign -vf 4 bin/AndroidTest.unsigned.unalign.apk bin/AndroidTest.unsigned.apk
 
 echo "APKSIGNER"
-
 apksigner sign  -v --ks MainKeystore.keystore --ks-key-alias MainKey --ks-pass pass:$Storepass --out bin/AndroidTest.apk bin/AndroidTest.unsigned.apk 
 
 echo "ADB"
-
 adb install -r bin/AndroidTest.apk
+
 if ! $SeeTemporaryFolder ; then
 	rm -fr bin/
 fi
