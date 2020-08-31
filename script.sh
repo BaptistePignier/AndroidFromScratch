a63f4da2f1570026528376bcec96409a22549f0a
miradox <pignier.baptiste@gmail.com>
2020-08-31 22:50:52 +0200

    change

--- script.sh
+++ script.sh
@@ -23,16 +23,14 @@ fi
 echo "AAPT2"
 mkdir -p gen bin
 aapt2 compile -v --dir res/ -o res/res.zip
-#aapt2 compile -v --dir libs/constraint-layout-1.1.3-res -o libs/constraint-layout-1.1.3-res.zip
-aapt2 link -v -I android.jar  --auto-add-overlay --manifest src/AndroidManifest.xml --java gen/ -o bin/AndroidTest.unsigned.unalign.apk res/res.zip 
-#-R libs/constraint-layout-1.1.3-res.zip
+aapt2 compile -v --dir libs/constraint-layout-1.1.3-res -o libs/constraint-layout-1.1.3-res.zip
+aapt2 link -v -I android.jar  -R libs/constraint-layout-1.1.3-res.zip --auto-add-overlay --manifest src/AndroidManifest.xml --java gen/ --extra-packages android.support.constraint -o bin/AndroidTest.unsigned.unalign.apk res/res.zip 
+
 
 #Find all usefull files
 thereislibs=$(find libs -type f -name '*.jar')
-if [ ! -z  $thereislibs ]; then
-	echo "aaaaaaa"
+if [ ! -z "thereislibs" ]; then
 	for i in libs/*.jar; do
-		echo $i
 		libs="$libs:$i"
 		dxlibs="$dxlibs $i"
 		d8libs="$d8libs --classpath $i"
@@ -46,10 +44,14 @@ all_kotlin=$(find src -type f -name '*.kt')
 all_code_java=$(find src -type f -name '*.java')
 
 
+
+
+# Compile all .java file. If there is no java app's files, this will compile only R.java needed by d8 command
 echo "JAVAC"
 mkdir -p obj
-if [ ! -z  $thereislibs ]; then
-	javac -bootclasspath android.jar -d obj/ -classpath $libs -sourcepath gen:src $all_R_java $all_code_java
+if [ ! -z  "$thereislibs" ]; then
+	echo $libs
+	javac -bootclasspath android.jar -d obj/ -classpath $libs:android.jar -sourcepath gen:src $all_R_java $all_code_java
 else 
 	javac -bootclasspath android.jar -d obj/ -sourcepath gen:src $all_R_java $all_code_java
 fi
